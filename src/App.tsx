65747f75c0fb130448befeed2c8b08eb4675afb4
hanafiabdilah <55283082+hanafiabdilah@users.noreply.github.com>
2023-01-26 09:55:49 +0700

    use state hook

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Input } from './components/Input';
 import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { LoggedIn } from './components/state/LoggedIn';
 import { Status } from './components/Status';
 
 function App() {
@@ -32,19 +33,7 @@ function App() {
 
   return (
     <div className="App">
-      <Container styles={{ padding: '1rem', border: '1px solid red' }} />
-      <Button handleClick={(event, id) => {
-        console.log('Button Clicked', event, id)
-      }} />
-      <Input value="" handleChange={event => console.log(event)} />
-      <Great name="Hanafi" messageCount={20} isLoggedIn={true} />
-      <Great name="Zahra" isLoggedIn={true} />
-      <Person name={personName} />
-      <PersonList names={names} />
-      <Status status="error" />
-      <Oscar>
-        <Heading>Placeholder Text</Heading>
-      </Oscar>
+      <LoggedIn />
     </div>
   );
 }
