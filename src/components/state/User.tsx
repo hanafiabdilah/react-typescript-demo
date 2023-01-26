dc1c2b80024b2fe46327ecda1568b9a77c8666e2
hanafiabdilah <55283082+hanafiabdilah@users.noreply.github.com>
2023-01-26 10:46:42 +0700

    use state assertion value

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,7 +6,8 @@ type AuthUser = {
 }
 
 export const User = () => {
-    const [user, setUser] = useState<AuthUser | null>(null);
+    // const [user, setUser] = useState<AuthUser | null>(null);
+    const [user, setUser] = useState<AuthUser>({} as AuthUser);
 
     const handleLogin = () => {
         setUser({
@@ -15,17 +16,17 @@ export const User = () => {
         });
     }
 
-    const handleLogout = () => {
-        setUser(null);
-    }
+    // const handleLogout = () => {
+    //     setUser(null);
+    // }
 
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
+            {/* <button onClick={handleLogout}>Logout</button> */}
             <div>
-                <div>user name is {user?.name}</div>
-                <div>user email is {user?.email}</div>
+                <div>user name is {user.name}</div>
+                <div>user email is {user.email}</div>
             </div>
         </div>
     )
